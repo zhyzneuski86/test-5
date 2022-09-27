6fe97cf97e5762a503eadf1a32b6bea3e42b3d2d
zhyzneuski86 <1293728@gmail.com>
2022-09-27 21:14:32 +0300

    commit 8

---
+++ script23.js
@@ -0,0 +1,10 @@
+let myDog
+myDog = {
+  // Only change code below this line
+  "name": "Scuby",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["people", "dog"]
+
+  // Only change code above this line
+}

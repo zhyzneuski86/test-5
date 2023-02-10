f525564897dec384091623d3ecc9f5bcbbb03d73
zhyzneuski86 <1293728@gmail.com>
2023-02-10 14:48:03 +0300

    commit 15

--- script35.js
+++ script35.js
@@ -3,4 +3,6 @@ const thirdLetterOfLastName = lastName[2];
 
 const firstName = "Lovelace";
 const lastLetterOfLastName = firstName[firstName.length - 1];
+const lastMyName = "Lovelace";
+const secondToLastLetterOfLastName = lastMyName[lastMyName.length - 2];
 

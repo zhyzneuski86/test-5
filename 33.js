5d0bcb6165eb8d1c75220e8751d14da0977d94af
zhyzneuski86 <1293728@gmail.com>
2023-02-10 13:29:45 +0300

    commit 10

---
+++ 33.js
@@ -0,0 +1,5 @@
+const myStrick = "This is the start. " + "This is the end.";
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+const myName = "Vadim";
+const myAnswer = "My name is " + myName + " and I am well!";

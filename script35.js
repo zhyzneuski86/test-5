ed1f555ffae1d28012083c1269d48d7bb37da598
zhyzneuski86 <1293728@gmail.com>
2023-02-10 15:36:23 +0300

    commit 21

--- script35.js
+++ script35.js
@@ -21,3 +21,10 @@ let myData = mySecondArray [0];
 const myThirdArray = [18, 64, 99];
 myThirdArray[0] = 45;
 
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+const myData = myArray[2][1];

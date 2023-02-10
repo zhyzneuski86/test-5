0296f86bc221beb91e7388135277c35575ba76de
zhyzneuski86 <1293728@gmail.com>
2023-02-10 14:18:17 +0300

    commit 11

--- 33.js
+++ 33.js
@@ -3,3 +3,11 @@ let myStr = "This is the first sentence. ";
 myStr += "This is the second sentence.";
 const myName = "Vadim";
 const myAnswer = "My name is " + myName + " and I am well!";
+
+const someAdjective = "good";
+let myExpression = "Learning to code is ";
+myExpression += someAdjective;
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+lastNameLength = lastName.length

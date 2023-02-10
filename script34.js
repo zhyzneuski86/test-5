6de0e694f3307b5e8b406d0ed6014a159f3d9428
zhyzneuski86 <1293728@gmail.com>
2023-02-10 14:33:05 +0300

    commit 12

---
+++ script34.js
@@ -0,0 +1,9 @@
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+firstLetterOfLastName = lastName[0];
+
+let myStr = "Jello World";
+myStr = "Hello World"
+
+const lastName = "Lovelace";
+const thirdLetterOfLastName = lastName[2];

d37617f110491e4f3c0271f8b3417e822e834ccf
zhyzneuski86 <1293728@gmail.com>
2023-02-11 20:20:06 +0300

    commit 58

--- script36.js
+++ script36.js
@@ -237,3 +237,35 @@ function testElseIf(val) {
 }
 
 testElseIf(7);
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  } else if (num < 10) {
+    return "Small";
+  } else if (num < 15) {
+    return "Medium";
+  } else if (num < 20) {
+    return "Large";
+  } else if (num >= 20) {
+    return "Huge";
+  } else {
+
+    return "Change Me";}
+  // Only change code above this line
+}
+
+testSize(7);

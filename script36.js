1ee4827fc4d146f663864ae350b487c1da6af6a5
zhyzneuski86 <1293728@gmail.com>
2023-02-11 19:59:21 +0300

    commit 57

--- script36.js
+++ script36.js
@@ -192,3 +192,48 @@ function testLogicalAnd(val) {
 }
 
 testLogicalAnd(10);
+
+function testLogicalOr(val) {
+  // Only change code below this line
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
+
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+  else {
+    return "Between 5 and 10";
+  }
+}
+
+testElseIf(7);

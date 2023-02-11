424d1ade5afabbb2906ac3c068e5a913089c1ab6
zhyzneuski86 <1293728@gmail.com>
2023-02-11 19:44:58 +0300

    commit 56

--- script36.js
+++ script36.js
@@ -149,3 +149,46 @@ function testGreaterOrEqual(val) {
 }
 
 testGreaterOrEqual(10);
+
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);

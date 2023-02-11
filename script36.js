78fedc6fa22d5233bba5dd61fd9b94e1250c116f
zhyzneuski86 <1293728@gmail.com>
2023-02-11 18:46:49 +0300

    commit 53

--- script36.js
+++ script36.js
@@ -95,3 +95,20 @@ function testStrict(val) {
 }
 
 testStrict(7);
+
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
+function testNotEqual(val) {
+  if (val != "99") { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);

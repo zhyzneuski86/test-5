244e428ec5eaf1deeb96a01608dbf1a4f64eb014
zhyzneuski86 <1293728@gmail.com>
2023-02-12 14:08:51 +0300

    commit 64

--- script37.js
+++ script37.js
@@ -104,3 +104,18 @@ function chainToSwitch(val) {
 }
 
 chainToSwitch(7);
+
+function isLess(a, b) {
+  return a < b;
+}
+isLess(10, 15);
+
+function abTest(a, b) {
+
+  if (a < 0 || b < 0) {
+    return undefined
+  };
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);

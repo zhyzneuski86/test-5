6ff4d23311a59a5e6644486e5a3e73e7b11ee202
zhyzneuski86 <1293728@gmail.com>
2023-02-12 14:23:52 +0300

    commit 65

--- script37.js
+++ script37.js
@@ -119,3 +119,31 @@ function abTest(a, b) {
 }
 
 abTest(2,2);
+
+let count = 0;
+
+function cc(card) {
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');

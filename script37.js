e0e8ae49cc5ba9c1ac41ed1fde02f49ad59a47fd
zhyzneuski86 <1293728@gmail.com>
2023-02-12 13:45:16 +0300

    commit 61

--- script37.js
+++ script37.js
@@ -37,3 +37,23 @@ function caseInSwitch(val) {
   }
 }
 caseInSwitch(1);
+
+function switchOfStuff(val) {
+  let answer = "";
+  switch (val) {
+    case "a":
+      return "apple";
+      break;
+    case "b":
+      return "bird";
+      break;
+    case "c":
+      return "cat";
+      break;
+    default:
+      return "stuff"
+  }
+
+}
+
+switchOfStuff(1);

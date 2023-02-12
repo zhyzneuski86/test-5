775516435371cd9723809e90f08978860003c478
zhyzneuski86 <1293728@gmail.com>
2023-02-12 16:10:29 +0300

    commit 70

--- script37.js
+++ script37.js
@@ -219,3 +219,31 @@ function phoneticLookup(val) {
 }
 
 phoneticLookup("charlie");
+
+unction checkObj(obj, checkProp) {
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+}
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    artist: "Deep Purple",
+    title: "Smoke on the water",
+    release_year: 1976,
+    formats: ["CD", "8T", "LP"]
+  }
+];

80a6d1d7e9cd6d3569ac18f6d3bb71e2373416a7
zhyzneuski86 <1293728@gmail.com>
2023-02-13 11:46:29 +0300

    Iterate Through an Array with a For Loop

--- script37.js
+++ script37.js
@@ -351,3 +351,9 @@ const myArray = [];
 for (let i = 9; i > 0; i -=2) {
   myArray.push(i)
 }
+
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i]
+}

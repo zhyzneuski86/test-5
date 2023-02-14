d6431764d27f0627e8fa754a84800bb4135a0913
zhyzneuski86 <1293728@gmail.com>
2023-02-14 19:32:19 +0300

    Stones on the Table

--- script37.js
+++ script37.js
@@ -464,3 +464,11 @@ function solve(arr){
   }
   return minMax;
 }
+function solve(stones) {
+  let count = 0
+  for (let i = 1; i <= stones.length; i++) {
+    if (stones[i] === stones[i-1]) { count++ }
+  }
+  return count
+}
+

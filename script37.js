5a1d0a9e429060ad1b888f39eebc0caf28e301c8
zhyzneuski86 <1293728@gmail.com>
2023-02-14 19:27:17 +0300

    Max-min arrays

--- script37.js
+++ script37.js
@@ -454,3 +454,13 @@ function validSpacing(s) {
 function solve(a,b){
   return (a+b).split("").filter(c => !a.includes(c) || !b.includes(c)).join("");
 }
+
+function solve(arr){
+  var minMax = [];
+  arr.sort((a,b)=>a-b);
+  for(var i = 0, j = arr.length-1; i <= j; i++,j--){
+    if(i != j) minMax.push(arr[j]), minMax.push(arr[i]);
+    else minMax.push(arr[i]);
+  }
+  return minMax;
+}

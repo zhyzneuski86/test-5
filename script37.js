78f2e019b0bfec84225a09fa2865c9e668a50028
zhyzneuski86 <1293728@gmail.com>
2023-02-14 19:22:35 +0300

    Unique string characters

--- script37.js
+++ script37.js
@@ -450,3 +450,7 @@ function validSpacing(s) {
   else
     return !s.split(" ").includes("")
 }
+
+function solve(a,b){
+  return (a+b).split("").filter(c => !a.includes(c) || !b.includes(c)).join("");
+}
